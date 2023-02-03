2d81708bd351a0f9b83e51c752f24b59be04fa37
zerbash <dbasham@overlake.org>
2023-02-03 12:00:31 -0800

    2-3-23

--- app/src/main/java/org/overlake/mat803/fragments/CountFragment.java
+++ app/src/main/java/org/overlake/mat803/fragments/CountFragment.java
@@ -1,7 +1,49 @@
 package org.overlake.mat803.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class CountFragment extends Fragment {
 
+    private int mCount;
+    private TextView mCountView;
+    public static final String INITIAL_COUNT = "initial_count";
+    public static final String REQUEST_KEY = "request_key";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCount = getArguments().getInt(INITIAL_COUNT);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_count, container, false);
+        mCountView = view.findViewById(R.id.count_view);
+        view.findViewById(R.id.increment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increment();
+            }
+        });
+        updateView();
+        return view;
+    }
+
+    public void increment() {
+        mCount++;
+        updateView();
+    }
+
+    public void updateView() {
+        mCountView.setText(Integer.toString(mCount));
+    }
 }
--- app/src/main/java/org/overlake/mat803/fragments/MainActivity.java
+++ app/src/main/java/org/overlake/mat803/fragments/MainActivity.java
@@ -1,6 +1,9 @@
 package org.overlake.mat803.fragments;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.os.Bundle;
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Bundle bundle = new Bundle();
+        bundle.putInt(CountFragment.INITIAL_COUNT, 5);
+        fm.beginTransaction()
+                .add(R.id.fragmentContainerView, CountFragment.class, bundle)
+                .commit();
+
+        fm.setFragmentResultListener(CountFragment.REQUEST_KEY, this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+
+            }
+        });
     }
 }
